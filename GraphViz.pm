f3b59de85460f9c56ce8470ee4d25afd0ecf2112
Michal Špaček <tupinek@gmail.com>
2014-12-06 20:55:58 +0100

    First real version.

--- GraphViz.pm
+++ GraphViz.pm
@@ -4,8 +4,111 @@ package Map::Tube::GraphViz;
 use strict;
 use warnings;
 
+# Modules.
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+use GraphViz2;
+use List::MoreUtils qw(none);
+use Readonly;
+use Scalar::Util qw(blessed);
+
+# Constants.
+Readonly::Array our @OUTPUTS => qw(text png);
+
 # Version.
 our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;
+
+	# Driver.
+	$self->{'driver'} = 'dot';
+
+	# Output.
+	$self->{'output'} = 'png';
+
+	# Map::Tube object.
+	$self->{'tube'} = undef;
+
+	# Process params.
+	set_params($self, @params);
+
+	# Check Map::Tube object.
+	if (! defined $self->{'tube'}) {
+		err "Parameter 'tube' is required.";
+	}
+	if (! blessed($self->{'tube'})
+		|| ! $self->{'tube'}->does('Map::Tube')) {
+
+		err "Parameter 'tube' must be 'Map::Tube' object.";
+	}
+
+	# Check output.
+	# TODO
+
+	# GraphViz object.
+	$self->{'_g'} = GraphViz2->new(
+		'global' => {
+			'directed' => 0,
+		},
+	);
+
+	# Object.
+	return $self;
+}
+
+# Get graph.
+sub graph {
+	my ($self, $output_file) = @_;
+	foreach my $node (values %{$self->{'tube'}->nodes}) {
+		my $node_links = split m/,/ms, $node->link;
+		my %params;
+		if ($node_links > 2) {
+			%params = (
+				'fillcolor' => 'yellow',
+				'style' => 'filled',
+				'shape' => 'diamond',
+			);
+		} elsif ($node_links == 1) {
+			%params = (
+				'fillcolor' => 'cyan',
+				'style' => 'filled',
+				'shape' => 'box',
+			);
+		}
+		$self->{'_g'}->add_node(
+			'label' => $node->name,
+			'name' => $node->id,
+			%params,
+		);
+	}
+	my @processed;
+	foreach my $node (values %{$self->{'tube'}->nodes}) {
+		foreach my $link (split m/,/ms, $node->link) {
+			if (none {
+				($_->[0] eq $node->id && $_->[1] eq $link) 
+				|| 
+				($_->[0] eq $link && $_->[1] eq $node->id)
+				} @processed) {
+
+				$self->{'_g'}->add_edge(
+					'from' => $node->id,
+					'to' => $link,
+				);
+				push @processed, [$node->id, $link];
+			}
+		}
+	}
+	return $self->{'_g'}->run(
+		'driver' => $self->{'driver'},
+		'format' => $self->{'output'},
+		'output_file' => $output_file,
+	);
+}
 
 1;
 
